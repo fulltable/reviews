e4262bb5a3061bee57e9016e1bd57a047b0d4e0f
Evan Kaplan <evankaplan@fb.com>
2019-05-03 18:20:58 -0700

    Remove loader io route handler

--- server/server.js
+++ server/server.js
@@ -15,10 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/restaurants/:id', express.static('client/dist'));
 app.use('/', express.static('client/dist'));
 
-app.get('/loaderio-3d3088ddc0ff5e3f736d9af2b3c81ff8/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/loaderio-3d3088ddc0ff5e3f736d9af2b3c81ff8.txt'));
-});
-
 app.post('/api/restaurants/:restaurant_id/reviews', controllers.post);
 app.get('/api/restaurants/:restaurant_id/reviews', controllers.get);
 app.patch('/api/restaurants/:restaurant_id/reviews/:review_id', controllers.patch);
